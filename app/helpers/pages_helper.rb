8ec924163a9571ebc63fe5671c6e48622e97ea75
sanght <hothanhsang2013@gmail.com>
2018-11-06 18:37:38 +0700

    First commit

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,5 @@
+module PagesHelper
+	def url_for_page(page)
+		page.slug.present? ? page.slug : "pages/#{page.id}"
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :pages, only: [:show]
+  namespace :admin do
+    resources :pages #/admin/pages
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  Page.where.not("slug", nil).all.each do |page|
+    get "/#{page.slug}", controller: "pages", action: "show" , id: page.id
+  end
+end
